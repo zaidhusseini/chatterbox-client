de0874180ee2664d687e1e174e44b4859ed683ef
zaidhusseini <zaidhusseini@gmail.com>
2018-01-26 21:28:22 -0800

    Implement index file with sorting params

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,28 @@
 // YOUR CODE HERE:
 
+
 var app = {};
+var roomsList = [];
+var currentRoom;
+var fetchedMessages;
+app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
 
 app.init = function init() {
   // body...
+  
 
+  app.clearMessages();
+  app.renderRoom('lobby');
+  //setInterval(app.fetch,1000);
+  app.fetch();
   $('.username').on('click', ()=>{
     app.handleUsernameClick();
   });
 
   $('.submit').on('submit', app.handleSubmit);
+
   
+
 
 };
 
@@ -18,7 +30,7 @@ app.send = function send(message) {
 
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     type: 'POST',
     data: message,
     contentType: 'application/json',
@@ -33,15 +45,17 @@ app.send = function send(message) {
 
 };
 
-app.fetch = function fetch(message) {
+app.fetch = function fetch() {
   // body...
   $.ajax({
-    url: undefined,
+    url: app.server,
     type: 'GET',
-    data: message,
+    data: 'order=-createdAt',
     contentType: 'application/json',
     success: function(data){
-      console.log('chatterbox: Message received');
+      fetchedMessages = data;
+      app.displayMessages();
+      console.log(fetchedMessages, 'chatterbox: Message received');
     },
     error: function(data){
       console.error('chatterbox: Failed to receive message', data);
@@ -64,6 +78,7 @@ app.renderMessage = function renderMessage(message) {
   //2) Add user name from message object to node
   //3) Add the text of message from object to node
   $message.text(message.username + ': ' + message.text);
+
   //4) Append that node to #chats node
   $('#main').append($chats);
   $chats.append($message);
@@ -74,16 +89,19 @@ app.renderRoom = function renderRoom(roomName) {
   var $room = $('<div></div>').attr('id', roomName);
   
   if ($('#roomSelect').children().length === 0) {
-    var $Div = $('<div></div>').attr('id','rooms');
+    roomsList.push(roomName);
+    var $rooms = $('<div>Room</div>').attr('id','rooms');
     var $roomSelector = $('<select></select>').attr('id','roomSelect');
     var $option = $(`<option value=${roomName}>${roomName}</option>`);
-    $('body').append($Div);
-    $('#roomSelect').append($roomSelector);
-    $roomSelector.append($option);
+    $('#main').prepend($rooms);
+    $('#rooms').append($roomSelector);
+    $('#roomSelect').append($option);
   } else {
     var $option = $(`<option value=${roomName}> ${roomName} </option>`);
     $('#roomSelect').append($option);
   }
+
+  currentRoom = roomName;
 
 };
 
@@ -94,4 +112,18 @@ app.handleUsernameClick = function(){
 app.handleSubmit = function(){
 
 };
+
+app.displayMessages = function () {
+  fetchedMessagesList = fetchedMessages.results;
+  var message = {};  
+
+  for(var i = 0; i < fetchedMessagesList.length; i++) {
+    message = fetchedMessagesList[i];
+    app.renderMessage(message);
+  }
+};
+
+
+$(document).ready(app.init);
+
 
